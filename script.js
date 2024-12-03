1fbe168a2b94b84d9e3961a5a4477a1528a5b45d
Sayed Sadiq <139980953+lmaotomato@users.noreply.github.com>
2024-12-03 17:23:29 +0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function openAchievements(department) {
-    const achievementsContent = document.getElementById('achievements-content');
-    achievementsContent.innerHTML = `<h2>${department.charAt(0).toUpperCase() + department.slice(1)} Achievements</h2><p>Details about achievements in the ${department} department.</p>`;
-    document.getElementById('achievements').style }
